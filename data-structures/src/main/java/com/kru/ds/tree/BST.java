b7823aa6525168976a43a8cf0c83e12f5dbaf683
Krunal Sabnis <krunalsabnis@gmail.com>
2018-02-04 17:24:03 +0530

    task-0001-1 Binary Search Tree add Delete Node method
    - delete for all three cases by finding successor
    - TO DO - Code Refractor and Optimize

--- data-structures/src/main/java/com/kru/ds/tree/BST.java
+++ data-structures/src/main/java/com/kru/ds/tree/BST.java
@@ -26,21 +26,92 @@ public class BST<T extends Comparable<T>> {
 
 
 	private Node<T> insertNode(Node<T> root2, T key) {
-
+		Node<T> newNode = null;
 		if (root2 == null)
 			root2 = new Node<T>(key);
-		else if (key.compareTo(root2.getKey()) > 0)
-			root2.setRight(insertNode(root2.getRight(), key));
-		else if (key.compareTo(root2.getKey()) < 0)
-			root2.setLeft(insertNode(root2.getLeft(), key));
+		else if (key.compareTo(root2.getKey()) > 0) {
+			newNode = insertNode(root2.getRight(), key);
+			root2.setRight(newNode);
+			newNode.setParent(root2);
+		} else if (key.compareTo(root2.getKey()) < 0) {
+			newNode = insertNode(root2.getLeft(), key);
+			root2.setLeft(newNode);
+			newNode.setParent(root2);
+			newNode.setLeftChild(true);
+		}
 		return root2;
 	}
 	
-	
+	public Node<T> search(T key) {
+		Node<T> result = search(root, key);
+		String printStatement = result == null ? "Key : " + key + " not found" : "Key : " + key + " " + result;
+		System.out.println(printStatement);
+		return result;
+	}
+
+	public Node<T> delete(T key) {
+		Node<T> node = search(key);
+		if (node == null) {
+			System.out.println("Nothing to delete");
+			return node;
+		}
+		
+		// Case 1: No Child for the node which is to be deleted
+		if (node.getLeft() == null && node.getRight() == null) {
+			node.getParent().setLeft(null);
+			node.getParent().setRight(null);
+			// Case 2: There is one child (left or right)
+		} else if (node.getLeft() == null) {	// Case 2.1 No Left Child only Right one is present
+			if (node.isLeftChild())
+				// if node is left child, attached Right one to Left of Parent
+				node.getParent().setLeft(node.getRight());	
+			else
+				// if node is Right child, attached Right one to Right of Parent
+				node.getParent().setRight(node.getRight());	
+		} else if (node.getRight() == null) {	// Case 2.2 No Right Child only Left one is present
+			if (node.isLeftChild())
+				node.getParent().setLeft(node.getLeft());
+			else
+				node.getParent().setRight(node.getLeft());
+		} else {
+			// Case 3. Node which is to be deleted has two children - Chose successor
+			Node<T> successor = node.getRight();
+			while(successor.getLeft() != null) {
+				successor = successor.getLeft();
+			}
+			successor.setLeft(node.getLeft());
+			if (successor.getKey() != node.getRight().getKey())
+				successor.setRight(node.getRight());
+			if (node.isLeftChild())
+				node.getParent().setLeft(successor);
+			else
+				node.getParent().setRight(successor);
+			
+			if (successor.isLeftChild())
+				successor.getParent().setLeft(null);
+			else
+				successor.getParent().setRight(null);
+			System.out.println("Successor for " + key + " is " + successor.getKey());
+
+		}
+		return node;
+	}
+
+	private Node<T> search(Node<T> root2, T key) {
+		if (root2 == null)
+			return null;
+		else if (key.compareTo(root2.getKey()) < 0)
+			return search(root2.getLeft(), key);
+		else if (key.compareTo(root2.getKey()) > 0)
+			return search(root2.getRight(), key);
+		else if (key.compareTo(root2.getKey()) == 0)
+			return root2;
+
+		return null;
+	}
 
 	private void traverse(Node<T> root1, boolean inorder) {
 		if (root1 != null) {
-			
 			Node<T> first = inorder ? root1.getLeft() : root1.getRight();			
 			Node<T> second = inorder ? root1.getRight() : root1.getLeft();
 
@@ -52,6 +123,5 @@ public class BST<T extends Comparable<T>> {
 	
 	public void traverse(boolean inorder) {
 		traverse(root, inorder);
-
 	}
 }
